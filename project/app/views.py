e2a3d88b3631a03ef5f10284b9713ce8d48b88ed
kuraiyamuna04 <yamunakurai78@gmail.com>
2023-09-23 11:04:37 +0530

    resolved mistakes

--- project/app/views.py
+++ project/app/views.py
@@ -45,8 +45,8 @@ class ProfileView(APIView):
 
         if not profile_serializer.is_valid():
             return Response(status=status.HTTP_404_NOT_FOUND)
+        profile_serializer.save()
         return Response(profile_serializer.data)
-
 
 
 class LoginView(APIView):
